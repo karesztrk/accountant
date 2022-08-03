625350a0adff4f9c95fc02c168fb62cf8da5065d
Károly Török <torok.karoly.krisztian@gmail.com>
2022-08-03 18:34:03 +0200

    Pie chart example

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -1,9 +1,171 @@
-import { Grid, SimpleGrid, Skeleton, useMantineTheme } from "@mantine/core";
+import {
+  Box,
+  Container,
+  Grid,
+  SimpleGrid,
+  Skeleton,
+  useMantineTheme,
+} from "@mantine/core";
 import Layout from "components/Layout";
 import React from "react";
+import { ResponsivePie } from "@nivo/pie";
 
 const Dashboard = () => {
-  return <Layout title="Dashboard" />;
+  const data = [
+    {
+      id: "lisp",
+      label: "lisp",
+      value: 470,
+      color: "hsl(335, 70%, 50%)",
+    },
+    {
+      id: "java",
+      label: "java",
+      value: 306,
+      color: "hsl(15, 70%, 50%)",
+    },
+    {
+      id: "sass",
+      label: "sass",
+      value: 491,
+      color: "hsl(90, 70%, 50%)",
+    },
+    {
+      id: "ruby",
+      label: "ruby",
+      value: 62,
+      color: "hsl(101, 70%, 50%)",
+    },
+    {
+      id: "php",
+      label: "php",
+      value: 374,
+      color: "hsl(356, 70%, 50%)",
+    },
+  ];
+  return (
+    <Layout title="Dashboard">
+      <Box sx={{ height: 400, width: "100%" }}>
+        <ResponsivePie
+          data={data}
+          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.5}
+          padAngle={0.7}
+          cornerRadius={3}
+          activeOuterRadiusOffset={8}
+          borderWidth={1}
+          borderColor={{
+            from: "color",
+            modifiers: [["darker", 0.2]],
+          }}
+          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsTextColor="#333333"
+          arcLinkLabelsThickness={2}
+          arcLinkLabelsColor={{ from: "color" }}
+          arcLabelsSkipAngle={10}
+          arcLabelsTextColor={{
+            from: "color",
+            modifiers: [["darker", 2]],
+          }}
+          defs={[
+            {
+              id: "dots",
+              type: "patternDots",
+              background: "inherit",
+              color: "rgba(255, 255, 255, 0.3)",
+              size: 4,
+              padding: 1,
+              stagger: true,
+            },
+            {
+              id: "lines",
+              type: "patternLines",
+              background: "inherit",
+              color: "rgba(255, 255, 255, 0.3)",
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10,
+            },
+          ]}
+          fill={[
+            {
+              match: {
+                id: "ruby",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "c",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "go",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "python",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "scala",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "lisp",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "elixir",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "javascript",
+              },
+              id: "lines",
+            },
+          ]}
+          legends={[
+            {
+              anchor: "bottom",
+              direction: "row",
+              justify: false,
+              translateX: 0,
+              translateY: 56,
+              itemsSpacing: 0,
+              itemWidth: 100,
+              itemHeight: 18,
+              itemTextColor: "#999",
+              itemDirection: "left-to-right",
+              itemOpacity: 1,
+              symbolSize: 18,
+              symbolShape: "circle",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemTextColor: "#000",
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </Box>
+    </Layout>
+  );
 };
 
 export default Dashboard;
