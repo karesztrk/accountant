10963472d7ae2a8f0c1d7b375b97559065aedf0d
Károly Török <torok.karoly.krisztian@gmail.com>
2022-08-04 14:19:15 +0200

    Navigation progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,49 @@ import {
   ColorSchemeProvider,
   MantineProvider,
 } from "@mantine/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { UserProvider } from "@supabase/auth-helpers-react";
 import { supabaseClient } from "@supabase/auth-helpers-nextjs";
 import { NotificationsProvider } from "@mantine/notifications";
+import { useRouter } from "next/router";
+import {
+  NavigationProgress,
+  resetNavigationProgress,
+  setNavigationProgress,
+  startNavigationProgress,
+} from "@mantine/nprogress";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      if (url !== router.asPath) {
+        startNavigationProgress();
+      }
+    };
+
+    const handleComplete = () => {
+      setNavigationProgress(100);
+      setTimeout(() => {
+        resetNavigationProgress();
+      }, 1000);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router.asPath]);
 
   return (
     <>
@@ -34,6 +68,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           withNormalizeCSS
           theme={{ colorScheme }}
         >
+          <NavigationProgress />
           <NotificationsProvider>
             <UserProvider supabaseClient={supabaseClient}>
               <Component {...pageProps} />
