0a7b814fb9de3a046a8a5bbf2dc8f1fcac23553a
Károly Török <torok.karoly.krisztian@gmail.com>
2022-09-25 14:45:37 +0200

    Custom fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import {
   ColorScheme,
   ColorSchemeProvider,
+  Global,
   MantineProvider,
 } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
@@ -10,6 +11,9 @@ import NavigationProgress from "components/navigation-progress/NavigationProgres
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { useState } from "react";
+
+const fontFamily = "Inter";
+const headingFontFamily = "Barlow";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
@@ -34,6 +38,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           withGlobalStyles
           withNormalizeCSS
           theme={{
+            fontFamily: `'${fontFamily}', sans-serif`,
+            headings: {
+              fontFamily: `'${headingFontFamily}', sans-serif`,
+              sizes: {
+                h1: { fontWeight: 900, fontSize: 44 },
+              },
+            },
             colorScheme,
             colors: {
               brand: [
@@ -53,6 +64,43 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
             focusRing: "always",
           }}
         >
+          <Global
+            styles={[
+              {
+                "@font-face": {
+                  fontFamily,
+                  src: "url(/Inter-latin-var.woff2) format('woff2')",
+                  fontWeight: 400,
+                  fontStyle: "normal",
+                  fontDisplay: "swap",
+                  unicodeRange:
+                    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD",
+                },
+              },
+              {
+                "@font-face": {
+                  fontFamily,
+                  src: "url(/Inter-latin-var.woff2) format('woff2')",
+                  fontWeight: 700,
+                  fontStyle: "normal",
+                  fontDisplay: "swap",
+                  unicodeRange:
+                    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD",
+                },
+              },
+              {
+                "@font-face": {
+                  fontFamily: headingFontFamily,
+                  src: "url(/Barlow-latin-black.woff2) format('woff2')",
+                  fontWeight: 900,
+                  fontStyle: "normal",
+                  fontDisplay: "swap",
+                  unicodeRange:
+                    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;",
+                },
+              },
+            ]}
+          />
           <NavigationProgress />
           <NotificationsProvider>
             <UserProvider supabaseClient={supabaseClient}>
